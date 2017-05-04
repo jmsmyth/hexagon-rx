66a4ae973b8243633ae24da0a7a142effcdc71c6
James Smyth <jmsmyth21@gmail.com>
2017-05-04 17:15:25 +0100

    Alwyas emit item-serializable-change

--- src/objects.js
+++ src/objects.js
@@ -34,7 +34,7 @@ export class Value extends Observable {
 export class Collection extends Observable {}
 
 class MappedValue extends Value {
-  constructor(originalValue, f) {
+  constructor (originalValue, f) {
     super()
     this.originalValue = originalValue
     this.f = f
@@ -198,14 +198,14 @@ export function mutableCollection (Type, options = {}) {
         this.map.set(t.id, t)
         this.listeners.set(t.id, changeListener)
 
-        if (serializable) {
-          const serializableChangeListener = (evt, eventMeta) => {
-            this.emit('item-serializable-change', evt, eventMeta)
+        const serializableChangeListener = (evt, eventMeta) => {
+          this.emit('item-serializable-change', evt, eventMeta)
+          if (serializable) {
             this.emit('serializable-change', this, eventMeta)
           }
-          this.serializableListeners.set(t.id, serializableChangeListener)
-          t.on('serializable-change', serializableChangeListener)
         }
+        this.serializableListeners.set(t.id, serializableChangeListener)
+        t.on('serializable-change', serializableChangeListener)
       })
       this.serializable = serializable
     }
@@ -231,14 +231,14 @@ export function mutableCollection (Type, options = {}) {
         this.map.set(v.id, v)
         this.listeners.set(v.id, changeListener)
 
-        if (serializable) {
-          const serializableChangeListener = (evt, eventMeta) => {
-            this.emit('item-serializable-change', evt, eventMeta)
+        const serializableChangeListener = (evt, eventMeta) => {
+          this.emit('item-serializable-change', evt, eventMeta)
+          if (serializable) {
             this.emit('serializable-change', this, eventMeta)
           }
-          this.serializableListeners.set(v.id, serializableChangeListener)
-          v.on('serializable-change', serializableChangeListener)
         }
+        this.serializableListeners.set(v.id, serializableChangeListener)
+        v.on('serializable-change', serializableChangeListener)
       })
       this.emit('set', this, eventMeta)
       this.emit('change', this, eventMeta)
@@ -271,15 +271,15 @@ export function mutableCollection (Type, options = {}) {
           this.emit('item-add', newValue, eventMeta)
           this.emit('change', this, eventMeta)
 
-          if (serializable) {
-            const serializableChangeListener = (evt, eventMeta) => {
-              this.emit('item-serializable-change', evt, eventMeta)
+          const serializableChangeListener = (evt, eventMeta) => {
+            this.emit('item-serializable-change', evt, eventMeta)
+            if (serializable) {
               this.emit('serializable-change', this, eventMeta)
             }
-            this.serializableListeners.set(newValue.id, serializableChangeListener)
-            newValue.on('serializable-change', serializableChangeListener, eventMeta)
-            this.emit('serializable-change', this, eventMeta)
           }
+          this.serializableListeners.set(newValue.id, serializableChangeListener)
+          newValue.on('serializable-change', serializableChangeListener, eventMeta)
+          this.emit('serializable-change', this, eventMeta)
         }
       } else {
         throw new Error('The object passed to MutableCollection::add() does not have the expected type ' + Type)
@@ -296,8 +296,9 @@ export function mutableCollection (Type, options = {}) {
           this.emit('item-remove', value, eventMeta)
           this.emit('change', this, eventMeta)
 
+          oldValue.off('serializable-change', this.serializableListeners.get(value.id), eventMeta)
+
           if (serializable) {
-            oldValue.off('serializable-change', this.serializableListeners.get(value.id), eventMeta)
             this.emit('serializable-change', this, eventMeta)
           }
 
@@ -332,7 +333,6 @@ const reservedKeys = [
 export class RXObject extends Observable {}
 
 export function object (obj, options = {}) {
-
   const serializable = options.serializable !== false
   const keys = Object.keys(obj)
 

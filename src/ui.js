f6c4430c4a8e6d99dc7bcc36cb71c949d1da9791
James Smyth <jmsmyth21@gmail.com>
2018-08-30 18:05:53 +0100

    Just import the parts of hexagon-js needed

--- src/ui.js
+++ src/ui.js
@@ -1,31 +1,32 @@
-import hx from 'hexagon-js'
+import { div, span, detached, Selection } from 'hexagon-js'
 import { Value, Collection } from './objects'
 
 Value.prototype.div = function (cls) {
-  const selection = hx.div(cls).text(this.get())
+  const selection = div(cls).text(this.get())
   this.on('change', (evt) => selection.text(evt))
   return selection
 }
 
 Value.prototype.span = function (cls) {
-  const selection = hx.span(cls).text(this.get())
+  const selection = span(cls).text(this.get())
   this.on('change', (evt) => selection.text(evt))
   return selection
 }
 
 Value.prototype.input = function (cls) {
-  const selection = hx.detached('input').class(cls).value(this.get())
+  const selection = detached('input').class(cls).value(this.get())
   selection.on('blur', (evt) => this.set(selection.value()))
   this.on('change', (evt) => selection.text(evt))
   return selection
 }
 
-Collection.prototype.div = function (cls, component) {
-  const selection = hx.div(cls)
+Collection.prototype.div = function (cls, component, options = {}) {
+  const selection = div(cls)
   const components = new Map()
+  const collection = this
 
   function add (obj) {
-    const comp = component(obj, this)
+    const comp = component(obj, collection)
     components.set(obj.id, comp)
     selection.add(comp)
   }
@@ -53,8 +54,8 @@ Collection.prototype.div = function (cls, component) {
   return selection
 }
 
-const originalClassed = hx.Selection.prototype.classed
-hx.Selection.prototype.classed = function (cls, include) {
+const originalClassed = Selection.prototype.classed
+Selection.prototype.classed = function (cls, include) {
   if (include instanceof Value) {
     const onChange = () => {
       originalClassed.call(this, cls, include.get())
